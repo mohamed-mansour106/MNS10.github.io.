0a193f25b7bafde6c8f667c967809d29bf9c77b0
mohamed-mansour106 <70229821+mohamed-mansour106@users.noreply.github.com>
2025-05-12 23:04:09 +0400

    Add files via upload

---
+++ branchsites/Movies/path/search-bar/search.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchInput");
+    const courses = document.querySelectorAll(".course");
+
+    searchInput.addEventListener("focus", () => {
+      courses.forEach(div => div.classList.remove("hidden"));
+    });
+
+    searchInput.addEventListener("input", function () {
+      const searchTerm = searchInput.value.toLowerCase();
+      courses.forEach(course => {
+        const text = course.textContent.toLowerCase();
+        if (text.includes(searchTerm)) {
+          course.classList.remove("hidden");
+        } else {
+          course.classList.add("hidden");
+        }
+      });
+    });
+  });
